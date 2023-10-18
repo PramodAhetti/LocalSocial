22cde0fd0c62fd874fec8579347d339564c804dc
Pramod M Ahetti <131883173+PramodAhetti@users.noreply.github.com>
2023-10-18 20:13:11 +0530

    reduced radius

--- routes/api/post.js
+++ routes/api/post.js
@@ -15,7 +15,7 @@ router.post('/all', userauth, async (req, res) => {
         }
 
         // Convert radius from kilometers to degrees (for MongoDB's $geoWithin operator)
-        const radiusInDegrees =0.00002;
+        const radiusInDegrees =0.000002;
 
         // Perform a geospatial query to find posts within the specified radius
         let nearbyPosts = await posts.find({
